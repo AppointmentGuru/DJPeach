65628b1abf9d703e809c2c6bcc8f9c44d80ae832
toast38coza <info@38.co.za>
2017-09-30 09:53:48 +0200

    fix issue with adding str and int. Upgrades transaction admin

--- copyandpay/admin.py
+++ copyandpay/admin.py
@@ -4,6 +4,9 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from .models import CreditCard, Transaction, Product, UserProduct, ScheduledPayment
 
+class ScheduledPaymentInline(admin.TabularInline):
+    model = ScheduledPayment
+
 class ProductAdmin(admin.ModelAdmin):
     """ServiceAdmin"""
     list_display = ('title', 'description', 'currency', 'price')
@@ -14,6 +17,9 @@ class UserProductAdmin(admin.ModelAdmin):
 
 class TransactionAdmin(admin.ModelAdmin):
     list_display = ('transaction_id', 'card', 'status', 'result_code', 'result_description', 'is_initial', 'currency', 'price')
+    search_fields = ('transaction_id','registration_id',)
+    # list_filter = ('status', 'result_code', 'is_initial',)
+    inlines = [ScheduledPaymentInline]
 
 class CreditCardAdmin(admin.ModelAdmin):
     list_display = ('bin', 'cardholder_name', 'expiry_month', 'expiry_year', 'last_four_digits')
