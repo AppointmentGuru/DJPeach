7d235278b2894b0b0d0034d0f757e42297d18221
toast38coza <info@38.co.za>
2018-02-02 18:53:35 +0200

    Adds ScheduledPayment resource

--- copyandpay/api.py
+++ copyandpay/api.py
@@ -1,7 +1,7 @@
 from django.http import Http404
 from rest_framework import routers, serializers, viewsets, filters
 
-from .models import Transaction
+from .models import Transaction, ScheduledPayment
 
 class IsOwnerFilterBackend(filters.BaseFilterBackend):
     """
@@ -10,6 +10,12 @@ class IsOwnerFilterBackend(filters.BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
         return queryset.filter(owner_id=request.user.id)
 
+class IsCustomerFilterBackend(filters.BaseFilterBackend):
+    """
+    Return only objects owned by the current user
+    """
+    def filter_queryset(self, request, queryset, view):
+        return queryset.filter(customer__owner_id=request.user.id)
 
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
@@ -24,5 +30,18 @@ class TransactionViewSet(viewsets.ModelViewSet):
     filter_backends = (IsOwnerFilterBackend,)
 
 
+class ScheduledPaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ScheduledPayment
+        fields = '__all__'
+        ordering = ('-created_date',)
+
+class ScheduledPaymentViewSet(viewsets.ModelViewSet):
+    queryset = ScheduledPayment.objects.all().order_by('-created_date')
+    serializer_class = ScheduledPaymentSerializer
+    filter_backends = (IsCustomerFilterBackend,)
+
+
 router = routers.DefaultRouter()
 router.register(r'transactions', TransactionViewSet)
+router.register(r'scheduled-payments', ScheduledPaymentViewSet)
